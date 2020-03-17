6e8805e8ba46ca3894f8d69a43a31aae88686c8a
Bearmonster <colea.ursu.99@mail.com>
2020-03-17 16:28:08 +0200

    shopping cart model and shopping cart models

--- DrinkStore/Data/AppDbContext.cs
+++ DrinkStore/Data/AppDbContext.cs
@@ -16,5 +16,7 @@ namespace DrinkStore.Data
         public DbSet<Drink> Drinks { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
